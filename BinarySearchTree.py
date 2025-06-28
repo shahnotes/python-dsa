2e400ea34485cf96b70cedfd3d9fe0ff6bbe9e72
Harsh Shah <shahharsh.210995@gmail.com>
2025-06-28 18:09:25 +0530

    BFS & DFS

--- BinarySearchTree.py
+++ BinarySearchTree.py
@@ -40,3 +40,31 @@ class BinarySearchTree:
             else:
                 return True
         return False
+    
+    def BFS(self):
+        current_node = self.root
+        queue = []
+        results = []
+        queue.append(current_node)
+        while len(queue) > 0:
+            current_node = queue.pop(0)
+            results.append(current_node.value)
+            if current_node.left is not None:
+                queue.append(current_node.left)
+            if current_node.right is not None:
+                queue.append(current_node.right)
+        return results
+
+    def DFS_pre_order(self):
+        results = []
+
+        def traverse(current_node):
+            if current_node is not None:
+                results.append(current_node.value)
+                if current_node.left is not None:
+                    traverse(current_node.left)
+                if current_node.right is not None:
+                    traverse(current_node.right)
+
+        traverse(self.root)
+        return results
