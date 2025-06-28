1a85ffc9d3c3664bbe278f4e7e85bdb48ffaac52
Harsh Shah <shahharsh.210995@gmail.com>
2025-06-28 19:42:42 +0530

    Sorting algos

--- Sort.py
+++ Sort.py
@@ -14,3 +14,40 @@ def selection_sort(array):
         if min_index != i:
             array[i], array[min_index] = array[min_index], array[i]
     return array
+
+def insertion_sort(array):
+    for i in range(1, len(array)):
+        key = array[i]
+        j = i - 1
+        while j >= 0 and key < array[j]:
+            array[j + 1] = array[j]
+            array[j] = key
+            j -= 1
+    return array
+
+def merge(array1, array2):
+    result = []
+    i, j = 0, 0
+    while i < len(array1) and j < len(array2):
+        if array1[i] < array2[j]:
+            result.append(array1[i])
+            i += 1
+        else:
+            result.append(array2[j])
+            j += 1
+    while i < len(array1):
+        result.append(array1[i])
+        i += 1
+    while j < len(array2):
+        result.append(array2[j])
+        j += 1
+    return result
+
+def merge_sort(array):
+    if len(array) <= 1:
+        return array
+    middle = len(array) // 2
+    left = merge_sort(array[:middle])
+    right = merge_sort(array[middle:])
+
+    return merge(left, right)
