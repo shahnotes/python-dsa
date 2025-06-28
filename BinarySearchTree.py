74883004cfd2d1bbe2b1bc355be4af8f5e3419e2
Harsh Shah <shahharsh.210995@gmail.com>
2025-06-28 18:14:36 +0530

    DFS methods

--- BinarySearchTree.py
+++ BinarySearchTree.py
@@ -68,3 +68,31 @@ class BinarySearchTree:
 
         traverse(self.root)
         return results
+
+    def DFS_post_order(self):
+        results = []
+
+        def traverse(current_node):
+            if current_node is not None:
+                if current_node.left is not None:
+                    traverse(current_node.left)
+                if current_node.right is not None:
+                    traverse(current_node.right)
+                results.append(current_node.value)
+
+        traverse(self.root)
+        return results
+
+    def DFS_in_order(self):
+        results = []
+
+        def traverse(current_node):
+            if current_node is not None:
+                if current_node.left is not None:
+                    traverse(current_node.left)
+                results.append(current_node.value)
+                if current_node.right is not None:
+                    traverse(current_node.right)
+
+        traverse(self.root)
+        return results
