4404982d68f38f67766131778cac9285e5822098
Harsh Shah <shahharsh.210995@gmail.com>
2025-06-28 18:27:48 +0530

    Sorting algorithms

---
+++ Sort.py
@@ -0,0 +1,16 @@
+def bubble_sort(array):
+    for i in range(len(array) -1, 0, -1):
+        for j in range(i):
+            if array[j] > array[j + 1]:
+                array[j], array[j + 1] = array[j + 1], array[j]
+    return array
+
+def selection_sort(array):
+    for i in range(len(array) - 1):
+        min_index = i
+        for j in range(i + 1, len(array)):
+            if array[j] < array[min_index]:
+                min_index = j
+        if min_index != i:
+            array[i], array[min_index] = array[min_index], array[i]
+    return array
