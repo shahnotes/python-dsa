d9e061d78d91a8e0313765d43e6138c74d67dc99
Harsh Shah <shahharsh.210995@gmail.com>
2025-06-30 00:06:05 +0530

    Problems

---
+++ problems/HouseRobber.py
@@ -0,0 +1,7 @@
+def rob_house(nums):
+    rob, no_rob = 0, 0
+    for num in nums:
+        new_rob = no_rob + num
+        new_no_rob = max(no_rob, rob)
+        rob, no_rob = new_rob, new_no_rob
+    return max(rob, no_rob)
